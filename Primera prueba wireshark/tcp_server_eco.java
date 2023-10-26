1bf2ee79263a30c965e8a2161d46370e30d36088
Archerd6 <archerd@uma.es>
2023-10-26 23:10:24 +0200

    Update Wireshark v1.0

--- Primera prueba wireshark/tcp_server_eco.java
+++ Primera prueba wireshark/tcp_server_eco.java
@@ -1,15 +1,19 @@
 import java.io.*;
 import java.net.*;
 
-class EchoServerTCP {
-	public static void main(String args[]) {
+class tcp_server_eco
+{
+	public static void main(String args[])
+	{
 		String line;
 		int numero_puerto = 6543;
-		try {
+		try
+		{
 			ServerSocket sockfd = new ServerSocket(numero_puerto);
 			System.out.println("Inicio servidor " + sockfd);
 
-			while (true) {
+			while (true)
+			{
 				Socket newsockfd = sockfd.accept();
 				System.out.println("Nuevo cliente, socket " + newsockfd);
 
@@ -17,18 +21,22 @@ class EchoServerTCP {
 				PrintWriter out = new PrintWriter(
 						new BufferedWriter(new OutputStreamWriter(newsockfd.getOutputStream())), true);
 				boolean salir = false;
-				while (!salir) {
+				while (!salir)
+				{
 					line = in.readLine(); // lectura socket cliente
-					if (line != null) {
+					if (line != null)
+					{
 						out.println(line);
 					} // escritura socket cliente
-					else {
+					else
+					{
 						salir = true;
 					} // cierre socket cliente
 				}
 				newsockfd.close();
 			}
-		} catch (Exception e) {
+		} catch (Exception e)
+		{
 			e.printStackTrace();
 		}
 	}
