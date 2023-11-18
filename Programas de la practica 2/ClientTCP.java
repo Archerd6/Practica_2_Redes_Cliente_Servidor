c74e52d26c70e57b0de9b2663681ef6c1b1f2f46
Archerd6 <archerd@uma.es>
2023-11-18 23:20:27 +0100

    Fixed TCP problems

--- Programas de la practica 2/ClientTCP.java
+++ Programas de la practica 2/ClientTCP.java
@@ -14,11 +14,7 @@ public class ClientTCP
 		
 		try
 		{
-			/* COMPLETAR Crear socket y conectar con servidor */
-			System.out.println("Dame la IP del servidor:");
-			serverName = stdIn.readLine();
-			System.out.println("Dame el numero del puerto del servidor:");
-			portNumber = Integer.parseInt(stdIn.readLine());			
+			/* COMPLETAR Crear socket y conectar con servidor */	
 			InetAddress serverAddr = InetAddress.getByName(serverName);
 	        serviceSocket = new Socket(serverAddr, portNumber);
 	        System.out.println("Conexion local"+serverAddr);
@@ -26,7 +22,6 @@ public class ClientTCP
 			/*COMPLETAR Inicializar los flujos de entrada/salida del socket conectado en las variables PrintWriter y BufferedReader*/
 			in = new BufferedReader(new InputStreamReader(serviceSocket.getInputStream()));
 	        out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(serviceSocket.getOutputStream())), true);
-			/**/
 		}
 		catch (UnknownHostException e)
 		{
@@ -38,6 +33,7 @@ public class ClientTCP
 			System.err.println("Couldn't get I/O for " + "the connection to: " + serverName);
 			System.exit(1);
 		}
+		
 		System.out.println("STATUS: Conectado al servidor ");
 		
 		/* Obtener texto por teclado */
@@ -60,26 +56,27 @@ public class ClientTCP
 			/* COMPLETAR Recibir texto en echo enviado por el servidor a traves del flujo de entrada del socket conectado */
 			String echo = null;
 			
-			echo = in.readLine(); /* Leer linea de socket conectado en variable echo */
+			echo = in.readLine(); /* Leer linea del socket*/
 			
 			System.out.println("echo: " + echo); // Muestra por pantalla el eco recibido
 			
 			/* Leer texto de usuario por teclado */
 			System.out.println("Introduzca un texto a enviar (END para acabar)");
 			userInput = stdIn.readLine();
+			
 		} /* Fin del bucle de servicio en cliente */
 		
 		// Salimos porque el cliente quiere terminar la interaccion, ha introducido END
 		System.out.println("STATUS: El cliente quiere terminar el servicio");
 		
-		/* COMPLETAR Enviar END al servidor para indicar el fin deL SERVICIO */
+		/* COMPLETAR Enviar END al servidor para indicar el fin del servicio */
 		out.println(userInput);
 		
 		System.out.println("STATUS: Sending " + userInput); // Muestra por pantalla el texto enviado
-		System.out.println("STATUS: Waiting for the reply"); // Muestra por pantalla estado
+		System.out.println("STATUS: Waiting for the reply"); // Muestra por pantalla el estado
 		
 		/* COMPLETAR Recibir OK enviado por el servidor confirmando EL FIN DEL SERVICIO */
-		String ok = in.readLine(); // Leer linea de socket conectado en variable ok y comparar con "OK"
+		String ok = in.readLine(); // Leer linea del socket
 		
 		System.out.println("STATUS: Cerrando conexion " + ok); // Muestra por pantalla el eco recibido
 		/* COMPLETAR Cerrar flujos y socket */
